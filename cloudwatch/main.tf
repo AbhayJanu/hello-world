2c8d7afcc8f86954d469c40b0edfd31eb3b633a2
Shireesha <shireesha2@gmail.com>
2024-02-19 18:42:33 +1100

    Docker-hello-world

--- cloudwatch/main.tf
+++
@@ -1,5 +0,0 @@
-
-
-resource "aws_cloudwatch_log_group" "cloudwatch" {
-  name = "log-group-test"
-}
--- sns-topic/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "sns_topic_arn" {
-  description = "ARN"
-  value       = aws_sns_topic.sns.arn
-}
-
-output "sns_topic_name" {
-  description = "Name"
-  value       = aws_sns_topic.sns.name
-}
